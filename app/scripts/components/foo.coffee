7ac137e3217d7e962b766fa6ea8463fb10031763
Shinichi Tomita <shinichi.tomita@gmail.com>
2015-02-25 00:32:59 +0900

    move sayhello to Foo component

--- app/scripts/components/foo.coffee
+++ app/scripts/components/foo.coffee
@@ -3,6 +3,11 @@
 React = require "react"
 FooTmpl = require "./foo.rt"
 
+sayHello = require "../util/say-hello"
+
 module.exports = React.createClass
+  onClick: ->
+    window.alert sayHello(@props.children)
+
   render: ->
     FooTmpl.apply(@)
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,6 @@
 var React = require("react");
 var Root = require("./components/root");
 
-var sayHello = require("./util/say-hello");
-
 document.addEventListener("DOMContentLoaded", function() {
   React.render(React.createElement(Root, {}), document.body);
 });
